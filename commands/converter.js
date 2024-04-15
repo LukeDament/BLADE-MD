4e33a851be593e7ff2ebb526125dcb8bad202679
LukeDament <148967386+LukeDament@users.noreply.github.com>
2024-04-15 09:26:12 +0500

    Update converter.js

--- commands/converter.js
+++ commands/converter.js
@@ -160,16 +160,16 @@ cmd({
             pattern: "fancy",
             desc: "Makes stylish/fancy given text",
             category: "converter",
-            use: '56 Secktor',
+            use: '56 Hidden',
             react: "✅",
             filename: __filename
         },
         async(Void, citel, text) => {
             if (isNaN(text.split(" ")[0]) || !text) {
                 let text = tiny(
-                    "Fancy text generator\n\nExample: .fancy 32 Secktor\n\n"
+                    "Fancy text generator\n\nExample: /fancy 32 Mr_HiddenX\n\n"
                 );
-                listall("Secktor Bot").forEach((txt, num) => {
+                listall("Mr_HiddenX Bot").forEach((txt, num) => {
                     text += `${(num += 1)} ${txt}\n`;
                 });
                 return await citel.reply(text);
